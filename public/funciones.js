901f4d507e8096f862e98d3f57d730e551517b87
Jorge Octavio Guzmán Sánchez <jogs78@MacBook-Pro-de-Jorge-2.local>
2018-10-23 15:58:21 -0500

    CLASE 23 OCTUBRE

--- public/funciones.js
+++ public/funciones.js
@@ -1,3 +1,12 @@
 function agregafila(){
-	$('#tabla >tbody').append('<tr><td>..</td><td>..</td></tr>');
+	var nom2 = document.getElementById("texto_nom").value;
+	var nom  = $('#texto_nom').val();
+	var app = 	$('#texto_app').val();
+	$('#tabla >tbody').append('<tr><td class="col_nombre">' +nom2 + '</td><td>' +app+ '</td></tr>');
+}
+
+function borrar(){
+//	alert( this.innerText );
+	this.parentNode.remove();
+
 }
--- resources/views/dinamica.php
+++ resources/views/dinamica.php
@@ -1,6 +1,12 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+
+	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 	<script src="/funciones.js"></script>
 	<script>
@@ -15,7 +21,10 @@
 				}
 			});
 
-			$('#otro').click( agregafila )
+			$('#otro').click( agregafila );
+			$('#tabla').on("click", ".col_nombre", borrar );
+
+
 	});
 
 	</script>
@@ -27,7 +36,7 @@
 <hr>
 	Click en el segundo enlace <button id="segundo"> de aquí </button>
 <hr>
-<table id="tabla">
+<table id="tabla" class="table">
 		<thead>
 			<tr>
 				<th>NOMBRE</th>
@@ -36,16 +45,68 @@
 		</thead>
 		<tbody>
 			<tr>
-				<td>Juan</td>
+				<td class="col_nombre">Juan</td>
 				<td>Perez</td>
 			</tr>
 			<tr>
-				<td>Maria</td>
+				<td class="col_nombre">Maria</td>
 				<td>Gomez</td>
 			</tr>
 		</tbody>
 	</table>	
-	<button id="otro" >otro</button>
+	
+
+ <!-- Button trigger modal -->
+<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
+  Agregar
+</button>
+
+<!-- Modal -->
+<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+<div class="container">
+  <form>
+    <div class="form-group row">
+      <label for="inputEmail3" class="col-sm-2 col-form-label">NOMBRE</label>
+      <div class="col-sm-10">
+        <input type="text" class="form-control" id="texto_nom" placeholder="Nombre">
+      </div>
+    </div>
+    <div class="form-group row">
+      <label for="inputPassword3" class="col-sm-2 col-form-label">APELLIDO</label>
+      <div class="col-sm-10">
+        <input type="text" class="form-control" id="texto_app" placeholder="Apellido">
+      </div>
+    </div>
+  </form>
+</div>
+
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+      	<button id="otro"  class="btn btn-primary">otro</button>
+        <button type="button">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div>
+
 
 </body>
+
+
 </html>
+
+
+
+
+
+
