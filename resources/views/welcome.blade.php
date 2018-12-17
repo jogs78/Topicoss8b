f1f71c63924f570269d1eec1dd7d42fdf728f968
Jorge Octavio Guzmán Sánchez <jogs78@gmail.com>
2018-12-16 19:00:49 -0600

    TITULO REGISTRO

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -82,7 +82,7 @@
                 <div class="title m-b-md">
                     Proyecto de tópicos
                 </div>
-
+                REGISTRO
                 <div class="links">
                     <a href="https://laravel.com/docs">Documentation</a>
                     <a href="https://laracasts.com">Laracasts</a>
