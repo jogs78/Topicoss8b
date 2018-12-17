ca787f7ddac208fe7d3b13e7b2bc7225aadd8939
Jorge Octavio Guzmán Sánchez <jogs78@gmail.com>
2018-12-16 18:58:14 -0600

    HAST AQUI

--- app/Http/Models/carro.php
+++ app/Http/Models/carro.php
@@ -5,6 +5,9 @@ namespace App\Http\Models;
 use Illuminate\Database\Eloquent\Model;
 
 class carro extends Model {
+
+    public $timestamps =false;
+
 	public function aquien(){
 	  return $this->hasOne('App\Http\Models\usuario', 'id', 'propietario')
 		->withDefault( ['nombre'=>'Sin propietario']);
